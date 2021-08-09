dc460e31782e394e4521946c7644a1fb86c43269
Davi Josue Palmas <davipalmas@alunos.utfpr.edu.br>
2021-08-09 16:37:03 -0300

    Update ChatServer.java

--- WhatsUT_Server/src/whatsut/server/ChatServer.java
+++ WhatsUT_Server/src/whatsut/server/ChatServer.java
@@ -4,19 +4,17 @@ import java.rmi.*;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.*;
 
-/**
- * @author davi on 5/25/21
- * @project WhatsUT_Server
- */
-
-public class ChatServer extends UnicastRemoteObject implements ChatServerInt {
+public class ChatServer extends UnicastRemoteObject implements ChatServerInt
+{
 
     private Vector v = new Vector();
 
-    public ChatServer() throws RemoteException {
+    public ChatServer() throws RemoteException
+    {
     }
 
-    public boolean login(ChatClientInt cci) throws RemoteException {
+    public boolean login(ChatClientInt cci) throws RemoteException
+    {
         System.out.println(cci.getName() + "  se conectou...");
         cci.tell("Você se conectou com sucesso.");
         publish(cci.getName() + " acabou de conectar.");
@@ -24,18 +22,24 @@ public class ChatServer extends UnicastRemoteObject implements ChatServerInt {
         return true;
     }
 
-    public void publish(String s) throws RemoteException {
+    public void publish(String s) throws RemoteException
+    {
         System.out.println(s);
-        for (int i = 0; i < v.size(); i++) {
-            try {
+        for (int i = 0; i < v.size(); i++)
+        {
+            try
+            {
                 ChatClientInt tmp = (ChatClientInt) v.get(i);
                 tmp.tell(s);
-            } catch (Exception e) {
+            }
+            catch (Exception e)
+            {
             }
         }
     }
 
-    public Vector getConnected() throws RemoteException {
+    public Vector getConnected() throws RemoteException
+    {
         return v;
     }
 }
