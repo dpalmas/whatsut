0bcf1773ce0a6eeb26e81c47dcbbb1c6741e4bc1
Davi Josue Palmas <davipalmas@alunos.utfpr.edu.br>
2021-08-09 16:37:17 -0300

    Update ChatClientInt.java

--- WhatsUT_Server/src/whatsut/server/ChatClientInt.java
+++ WhatsUT_Server/src/whatsut/server/ChatClientInt.java
@@ -3,11 +3,6 @@ package whatsut.server;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
-/**
- * @author davi on 5/25/21
- * @project WhatsUT_Server
- */
-
 public interface ChatClientInt extends Remote 
 {
     public void tell(String name) throws RemoteException;
