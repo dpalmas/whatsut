2ab853ac4b42bc3dc155438d1ebbade266fa266c
Davi Josue Palmas <davipalmas@alunos.utfpr.edu.br>
2021-08-09 16:38:16 -0300

    Update StartServer.java

--- WhatsUT_Server/src/whatsut/server/StartServer.java
+++ WhatsUT_Server/src/whatsut/server/StartServer.java
@@ -3,14 +3,12 @@ package whatsut.server;
 import java.rmi.*;
 import java.rmi.server.*;
 
-/**
- * @author davi on 5/25/21
- * @project WhatsUT_Server
- */
-
-public class StartServer {
-    public static void main(String[] args) {
-        try {
+public class StartServer 
+{
+    public static void main(String[] args) 
+    {
+        try 
+        {
             java.rmi.registry.LocateRegistry.createRegistry(1099);
             ChatServerInt csi = new ChatServer();
             Naming.rebind("rmi://seu_ip_aqui/admin", csi);
