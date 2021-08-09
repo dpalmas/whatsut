6697523769733c3ad6d8752af8552b1412d5828b
Davi Josue Palmas <davipalmas@alunos.utfpr.edu.br>
2021-08-09 16:37:35 -0300

    Update ChatServerInt.java

--- WhatsUT_Server/src/whatsut/server/ChatServerInt.java
+++ WhatsUT_Server/src/whatsut/server/ChatServerInt.java
@@ -3,12 +3,8 @@ package whatsut.server;
 import java.rmi.Remote;
 import java.rmi.RemoteException;
 
-/**
- * @author davi on 5/25/21
- * @project WhatsUT_Server
- */
-
-public interface ChatServerInt extends Remote {
+public interface ChatServerInt extends Remote 
+{
     public boolean login(ChatClient cc) throws RemoteException;
 
     public void publish(String s) throws RemoteException;
